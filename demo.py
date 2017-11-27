53743475935cedc2b6471dc845efec3a034126cc
wangqihui <872081365@qq.com>
2017-11-27 09:23:42 +0800

    add scrapy

--- demo.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import requests
-proxies = {
-    "https": "https://172.16.21.63:1080"
-}
-response_index = requests.get("https://www.youtube.com/", proxies=proxies)
-print len(response_index.text)
